93585a343879fc7b48402b1946943b701c8e5e88
Shreykrv <121611284+Shreykrv@users.noreply.github.com>
2023-08-29 10:40:12 +0530

    Update settings.py

--- dams/settings.py
+++ dams/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-y9u_84d!^y274fah@f@3d*5c4xr8b4c26567b2+w5y^u1o=d_%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1' , '.dams.app']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
@@ -135,3 +136,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static'),
+
+
+STAICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build','static')
+
+STAICFILES_STORAGE = "whitenoise.storage.CompressedMainfestStaticFilesStorage"
